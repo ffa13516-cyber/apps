4092e059cf00faac8d1a2c25089ddec318010e7e
ffa13516-cyber <ffa13516@gmail.com>
2026-04-18 23:14:58 +0200

    Add username field to User data class

--- app/src/main/java/com/messenger/app/data/model/User.kt
+++ app/src/main/java/com/messenger/app/data/model/User.kt
@@ -4,6 +4,7 @@ data class User(
     val uid: String = "",
     val phoneNumber: String = "",
     val displayName: String = "",
+    val username: String = "",
     val avatarUrl: String = "",
     val lastSeen: Long = System.currentTimeMillis(),
     val isOnline: Boolean = false
@@ -12,6 +13,7 @@ data class User(
         "uid" to uid,
         "phoneNumber" to phoneNumber,
         "displayName" to displayName,
+        "username" to username,
         "avatarUrl" to avatarUrl,
         "lastSeen" to lastSeen,
         "isOnline" to isOnline
