a6a9ab3272dddc8ed76afafd189cfc7e6a778ddf
ffa13516-cyber <ffa13516@gmail.com>
2026-04-18 22:36:48 +0200

    Add SettingsFragment for user settings management

---
+++ app/src/main/java/com/messenger/app/ui/home/SettingsFragment.kt
@@ -0,0 +1,39 @@
+package com.messenger.app.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.messenger.app.databinding.FragmentSettingsBinding
+import com.messenger.app.ui.auth.LoginActivity
+import com.messenger.app.utils.SessionManager
+
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var sessionManager: SessionManager
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sessionManager = SessionManager.getInstance(requireContext())
+
+        binding.btnLogout.setOnClickListener {
+            sessionManager.clearSession()
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            requireActivity().finish()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
